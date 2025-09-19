97591c5cdb25781e5481b36e45f1c60d4e5247b4
Sabin Ciurusniuc <saciurus@f2r8s17.clusters.42paris.fr>
2025-09-19 15:34:05 +0200

    done

--- sleep_and_think.c
+++ sleep_and_think.c
@@ -6,7 +6,7 @@
 /*   By: saciurus <saciurus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 15:12:24 by saciurus          #+#    #+#             */
-/*   Updated: 2025/09/16 19:15:45 by saciurus         ###   ########.fr       */
+/*   Updated: 2025/09/19 15:33:54 by saciurus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ void	think_philo(t_philo *philo, long start_time)
 	if (!data->stop)
 		printf("%ld %d is thinking\n", get_time_ms() - start_time, philo->id);
 	pthread_mutex_unlock(&data->print_mutex);
-	end = get_time_ms();
+	end = get_time_ms() + 50;
 	while (!data->stop && get_time_ms() < end)
 		usleep(1000);
 	if (data->stop)
